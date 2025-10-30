a27e6ed1b97752e6641ca9735099217822c227d9
machiaveeli <felipecaalamari@Hotmail.com>
2025-10-30 18:37:00 -0300

    commit inicial do projeto

---
+++ SistemaCadastroUsuarios/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCadastroUsuarios.Models
+{
+    public class Usuario : Pessoa
+    {
+        public string Login {  get; set; }
+        public string Senha { get; set; }
+
+        public Usuario(string nome, DateTime dataNascimento, string cpf, string login, string senha)
+            :base(nome, dataNascimento, cpf)
+        { 
+            Login = login;
+            Senha = senha;
+        }
+    }
+}
